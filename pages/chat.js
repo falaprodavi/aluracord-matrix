748acf307f3c37499d665b57909ec9cdced1c5e0
falaprodavi <falaprodavi@gmail.com>
2022-01-26 17:40:33 -0300

    Aula 03  React - Alura

--- pages/chat.js
+++ pages/chat.js
@@ -1,5 +1,199 @@
-export default function PaginaDoChat () {
-    return (
-        <div> Página do Chat </div> 
-    )
+import { Box, Text, TextField, Image, Button } from "@skynexui/components";
+import React from "react";
+import appConfig from "../config.json";
+
+export default function ChatPage() {
+  const [mensagem, setMensagem] = React.useState("");
+  const [listaDeMensagens, setListaDeMensagens] = React.useState([]);
+  // Sua lógica vai aqui
+  function handleNovaMensagem(novaMensagem) {
+    const mensagem = {
+      id: listaDeMensagens.length + 1,
+      de: "Davi Firmino",
+      texto: novaMensagem,
+    };
+    setListaDeMensagens([mensagem, ...listaDeMensagens]);
+    setMensagem("");
+  }
+  // ./Sua lógica vai aqui
+  return (
+    <Box
+      styleSheet={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: appConfig.theme.colors.primary[500],
+        backgroundImage: `url(https://images.pexels.com/photos/3233371/pexels-photo-3233371.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundBlendMode: "multiply",
+        color: appConfig.theme.colors.neutrals["000"],
+      }}
+    >
+      <Box
+        styleSheet={{
+          display: "flex",
+          flexDirection: "column",
+          flex: 1,
+          boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
+          borderRadius: "5px",
+          backgroundColor: appConfig.theme.colors.neutrals[700],
+          height: "100%",
+          maxWidth: "70%",
+          maxHeight: "70vh",
+          padding: "32px",
+        
+        }}
+      >
+        <Header />
+        <Box
+          styleSheet={{
+            position: "relative",
+            display: "flex",
+            flex: 1,
+            height: "80%",            
+            flexDirection: "column",
+            borderRadius: "5px",
+            padding: "16px",
+            fontSize: "20px",
+          }}
+        >
+          <MessageList mensagens={listaDeMensagens} />
+          {/* {listaDeMensagens.map((mensagemAtual) => {
+            return (
+            <li key={mensagemAtual.id}>
+                {mensagemAtual.de}: {mensagemAtual.texto}</li>
+          })} */}
+          <Box
+            as="form"
+            styleSheet={{
+              display: "flex",
+              alignItems: "center",
+              
+              
+            }}
+          >
+            <TextField
+              value={mensagem}
+              onChange={(event) => {
+                const valor = event.target.value;
+                setMensagem(valor);
+              }}
+              onKeyPress={(event) => {
+                if (event.key === "Enter") {
+                  event.preventDefault();
+                  handleNovaMensagem(mensagem);
+                }
+              }}
+              placeholder="Insira sua mensagem aqui..."
+              type="textarea"
+              styleSheet={{
+                width: "100%",
+                border: "0",
+                resize: "none",
+                borderRadius: "5px",
+                padding: "6px 8px",
+                backgroundColor: appConfig.theme.colors.primary[100],
+                color: appConfig.theme.colors.neutrals[999],
+                fontSize: "20px",                
+              }}
+            />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+function Header() {
+  return (
+    <>
+      <Box
+        styleSheet={{
+          width: "100%",
+          marginBottom: "16px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",          
+        }}
+      >
+        <Text variant="heading5">Chat</Text>
+        <Button
+          variant="tertiary"
+          color="read"
+          label="Logout"
+          href="/"
+        />
+      </Box>
+    </>
+  );
+}
+
+function MessageList(props) {
+  console.log("MessageList", props);
+  return (
+    <Box
+      tag="ul"
+      styleSheet={{
+        display: "flex",
+        flexDirection: "column-reverse",
+        flex: 1,
+        color: appConfig.theme.colors.neutrals["100"],
+        marginBottom: "16px",
+        
+        
+      }}
+    >
+      {props.mensagens.map((mensagem) => {
+         return (
+            <Text
+            key={mensagem.id}
+        tag="li"
+        styleSheet={{
+          borderRadius: "5px",
+          padding: "6px",
+          marginBottom: "12px",
+          fontSize: "20px",
+          border: "1px",
+          hover: {
+            backgroundColor: appConfig.theme.colors.neutrals[800],
+          },
+        }}
+      >
+        <Box
+          styleSheet={{
+            marginBottom: "8px",
+          }}
+        >
+          <Image
+            styleSheet={{
+              width: "40px",
+              height: "40px",
+              borderRadius: "50%",
+              display: "inline-block",
+              marginRight: "8px",
+            }}
+            src={`https://github.com/falaprodavi.png`}
+          />
+          <Text tag="strong">
+              {mensagem.de}
+          </Text>
+          <Text
+            styleSheet={{
+              fontSize: "10px",
+              marginLeft: "8px",
+              color: appConfig.theme.colors.neutrals[300],
+            }}
+            tag="span"
+          >
+            {new Date().toLocaleDateString()}
+          </Text>
+        </Box>
+        {mensagem.texto}
+      </Text>
+         );
+      })}  
+      
+    </Box>
+  );
 }
